67216e80faab205ad26d5e55976c032be528e21e
Jailton SP <jailton.sp@hotmail.com>
2024-05-31 11:46:16 -0300

    Exemplo 04

---
+++ ex004/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e constantes</title>
+</head>
+<body>
+    <H1>Variáveis e constantes</H1>
+
+    <p>O que são variáveis e constantes?</p>
+    
+    <p>Variáveis: </p>
+    <p>As variáveis recebe o $ e o nome da variável</p>
+    <p>Declaração de varíável</p>
+    //$nome = "Jailton";
+    //$sobrenome = "Pereira";
+    //
+    //
+    
+    <p>Exemplo:</p>
+    <?php 
+    $nome = "Jailton";
+    $sobrenome = "Pereira";
+
+    echo "Muito Prazer, $nome $sobrenome!";
+    ?>
+    <p>Constantes:</p>
+
+    <p></p>
+
+</body>
+</html>
